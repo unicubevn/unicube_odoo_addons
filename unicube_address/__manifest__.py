3adf464a2cd0c14c4bb3f8f0d8a0bf47ee9680e8
Jean Nguyen <community@thebeanfamily.org>
2023-11-16 07:23:53 +0700

    init v17.0 modules

---
+++ unicube_address/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Utilities: Vietnamese Address based on Circular No 124/2004/QĐ-TTg issued by Viet Nam Government (for backend sites only)",
+
+    'summary': """
+        This is the Vietnamese address module based on Circular No 124/2004/QĐ-TTg issued by Viet Nam Government (for backend sites only)
+            """,
+
+    'description': """
+        This is the Vietnamese address module based on Circular No 124/2004/QĐ-TTg issued by Viet Nam Government (for backend sites only)
+    """,
+
+    'author': "The Bean Family (an UniCube member)",
+    "license": "LGPL-3",
+    'website': "https://unicube.vn",
+    'category': 'UniCube/Utilities',
+    'version': '17.0.0.1',
+
+    "application": True,
+    "installable": True,
+    "images": ["static/description/vietnamese-addresses.png"],
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner.xml',
+        'views/res_address_menu.xml',
+        'views/res_company_info.xml',
+        'data/res.country.state.xml',
+        'data/res.country.district.csv',
+        'data/res.country.ward.csv'
+    ],
+
+}
---
+++ unicube_bank/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import account_move
+from . import res_bank
+from . import res_partner_bank
+from . import res_company
